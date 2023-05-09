abdeb39c4636d0e3a1184e992efff0051d0f3839
Rodrigo Gomez Palacio <rogomezpalacio@protonmail.com>
2023-05-09 15:43:15 -0500

    0.2.4 Release

--- src/bowserCastle.ts
+++ src/bowserCastle.ts
@@ -1,28 +1,22 @@
-import * as Bowser from "bowser";
+import Bowser from "bowser";
 
-export interface IBowserCastle extends Bowser.Parser.Details {
+export interface IBowserCastle {
   mobile: boolean;
   tablet: boolean;
+  name: string;
+  version: string;
 }
 
-const detect = (userAgent: string): Bowser.Parser.Details & { mobile: boolean; tablet: boolean; } => {
+const detect = (userAgent: string): IBowserCastle => {
   const browserInfo = Bowser.getParser(userAgent ?? '');
-  const bowserCastle = {
-    ...browserInfo,
+  return {
     mobile: browserInfo.getPlatformType(true) === "mobile",
     tablet: browserInfo.getPlatformType(true) === "tablet",
+    name: browserInfo.getBrowserName(),
+    version: browserInfo.getBrowserVersion(),
   };
-  return bowserCastle as Bowser.Parser.Details & { mobile: boolean; tablet: boolean; };
 }
 
-(function(root: any, name: string, definition: () => Bowser.Parser.Details & { mobile: boolean; tablet: boolean; }) {
-  if (typeof module !== 'undefined' && module.exports) {
-    module.exports = definition();
-  } else if (typeof define === 'function' && define.amd) {
-    define(name, definition);
-  } else {
-    root[name] = definition();
-  }
-})(this, 'bowserCastle', () => {
-  return detect(typeof navigator !== 'undefined' ? navigator.userAgent : '') as Bowser.Parser.Details & { mobile: boolean; tablet: boolean; };
-});
+const bowserCastle: IBowserCastle = detect(typeof navigator !== 'undefined' ? navigator.userAgent : '');
+
+export default bowserCastle;
